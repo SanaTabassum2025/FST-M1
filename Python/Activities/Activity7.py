704c26172d04922a2054e5978497d35eafa39d37
SanaTabassum2025 <sstabassum2898@gmail.com>
2025-12-30 19:39:01 +0530

    Delete Python/Activities/Activity7.py

--- Python/Activities/Activity7.py
+++
@@ -1,7 +0,0 @@
-list = input("Enter your sequence of list with commas:").split(",")
-
-sumofList = 0
-for number in list:
-    sumofList += int(number)
-
-print("Sum of the numbers of list:" +str(sumofList))
