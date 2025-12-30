da6880bd5b14ae68d2c7440b117cfefb977cd5d1
SanaTabassum2025 <sstabassum2898@gmail.com>
2025-12-30 19:39:10 +0530

    Delete Python/Activities/Activity8.py

--- Python/Activities/Activity8.py
+++
@@ -1,9 +0,0 @@
-list = input("Enter your sequence of list with commas:").split(",")
-
-firstnumber = list[0].strip() #.strip() removes any whitespace around the item
-lastnumber = list[-1].strip()
-
-if (firstnumber == lastnumber):
-    print(True)
-else:
-    print(False)
